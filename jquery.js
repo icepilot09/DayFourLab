19677334704a3541b3241fe0955ea9cde3f5d988
[Michael] <michael@lewiscommunications.com>
2015-09-11 11:48:44 -0500

    hid uppercase

--- jquery.js
+++ jquery.js
@@ -1,1 +1,8 @@
-$
+$(document).ready(function() {
+	$('#keyboard-upper-container').hide(function (){	
+	});
+	$('#keyboard-upper-container').keydown(function(){
+		$('keyboard-upper-container').show(function(){			
+		});
+	});
+});
