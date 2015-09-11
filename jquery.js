4f26bfa205d8f0829f43851f82a7f19ec6ace4e3
[Michael] <michael@lewiscommunications.com>
2015-09-11 14:59:32 -0500

    matched the keys to ascii

--- jquery.js
+++ jquery.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 			$('#keyboard-lower-container').hide();
 			$('#keyboard-upper-container').show();
 		};
-});
+	});
 	$(document).keyup(function(e){
 		if (e.which === 16) {
 			$('#keyboard-upper-container').hide();
@@ -14,5 +14,27 @@ $(document).ready(function() {
 		};
 	
 	});
+	$(document).keypress(function(e){
+    var s = String.fromCharCode(e.which);
+    if (s.match(/[a-zA-Z]/),(/[32]/))
+        console.log(s + ' is a match!');
+	});
+	/*var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+	var lines = [];
+	var lineCount = 0;
+	$(sentences).each(function(){
+    var letters = this.split('');
+    console.log(letters);
+    var line = $('<div class="letters"></div>');
+    $(letters).each(function() {
+       var letter = this;
+       if(letter == ' ') {
+           letter = '&nbsp;';
+      	 }
+       	line.append($('<div class="letter"></div>').html(letter));
+   	});
+   	lines.push(line);
+	});
 
+	$('.test').append(lines[lineCount]);*/
 });
